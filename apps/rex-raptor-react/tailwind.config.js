23cda1f95d4912252207b541c872033ee6609124
Santiago Torres Aragon <contact@santiagotorres.me>
2024-04-17 09:57:44 -0500

    Init

--- apps/rex-raptor-react/tailwind.config.js
+++ apps/rex-raptor-react/tailwind.config.js
@@ -1,5 +1,6 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
+const config = require( "../tailwindstyles/tailwind.config");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -9,6 +10,7 @@ module.exports = {
       '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
+    "../tailwindstyles/tailwind.config.js"
   ],
   theme: {
     extend: {},
